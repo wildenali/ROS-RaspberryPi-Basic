5712ae380cbfac2205d0f44182964b866feee601
wildenali <wildenwildenaliali@gmail.com>
2020-10-13 22:35:21 +0700

    Create a sub.py node

---
+++ ae_publisher_and_subscriber_python/src/sub.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def callback(pesan):
+	# get called id(): Get fully resolve name of local node
+	rospy.loginfo(rospy.get_caller_id() + "Berhitung woy %s", pesan.data)
+
+def listener():
+	rospy.init_node('py_subscriber', anonymous=True)
+	rospy.Subscriber('py_kirim', String, callback)
+	
+	rospy.spin()
+
+if __name__ == '__main__':
+	listener()
