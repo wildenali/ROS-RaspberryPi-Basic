3949693d6c3c78899c016a5f49c7689b742393f0
wildenali <wildenwildenaliali@gmail.com>
2021-06-08 16:37:40 +0700

    ACTION - How to send goal status between SUCCEEDED or ABORTED

---
+++ an_action_count_until/src/d_count_until_client_goal.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import rospy
+import actionlib
+
+from an_action_count_until.msg import CountUntilAsyncAction, CountUntilAsyncGoal
+
+class CountUntilClient:
+	def __init__(self):
+		self._ac = actionlib.SimpleActionClient('/count_until_goal', CountUntilAsyncAction)
+		self._ac.wait_for_server()
+		rospy.loginfo("Action Server Goal is up, we can send new goals!")
+	
+	def send_goal_and_get_result(self):
+		goal = CountUntilAsyncGoal(max_number=10, wait_duration=0.5)	# coba ganti2 nilainya, misal max_number nya dari 5 15 dan 20
+		self._ac.send_goal(goal, done_cb=self.done_callback, feedback_cb=self.feedback_callback)	# create feedback
+		rospy.loginfo("Goal has been sent")
+	
+	def done_callback(self, status, result):
+		rospy.loginfo("Status is : " + str(status))
+		rospy.loginfo("Result is : " + str(result))
+		
+	def feedback_callback(self, feedback):		# callback feeedback
+		rospy.loginfo(feedback)
+		rospy.loginfo("persentasinya: " + str(feedback.percentage))
+	
+if __name__ == '__main__':
+	rospy.init_node('count_until_client_goal')
+	
+	client = CountUntilClient()
+	
+	client.send_goal_and_get_result()
+	
+	rospy.spin()
---
+++ an_action_count_until/src/d_count_until_server_goal.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import rospy
+import actionlib
+
+from an_action_count_until.msg import CountUntilAsyncAction
+from an_action_count_until.msg import CountUntilAsyncGoal
+from an_action_count_until.msg import CountUntilAsyncResult
+from an_action_count_until.msg import CountUntilAsyncFeedback
+
+class CountUntilServer:
+  def __init__(self):
+    self._as = actionlib.SimpleActionServer('/count_until_goal', CountUntilAsyncAction, execute_cb=self.on_goal, auto_start=False)
+    self._as.start()
+
+    self._counter = 0
+    rospy.loginfo("Simple Action Goal Server has been started")
+  
+  def on_goal(self, goal):
+    rospy.loginfo("A goal has been received!")
+    rospy.loginfo(goal)
+
+    max_number = goal.max_number
+    wait_duration = goal.wait_duration
+
+    self._counter = 0
+    rate = rospy.Rate(1.0/wait_duration)
+    
+    success = False
+    
+    while not rospy.is_shutdown():
+      self._counter += 1
+      if self._counter > 9:		# coba di ...action.py file nya ganti jadi angka lebih dari 20 misalnya
+        succces = False
+        break
+      if self._counter >= max_number:
+        success = True
+        break
+      rospy.loginfo(self._counter)
+      feedback = CountUntilAsyncFeedback()								# create feedback
+      feedback.percentage = float(self._counter) / float(max_number)	# percentage feedback
+      self._as.publish_feedback(feedback)
+      rate.sleep()
+
+    result = CountUntilAsyncResult()
+    result.count = self._counter
+    rospy.loginfo("Send goal result to client")
+    
+    if success:
+      rospy.loginfo("Sukses")
+      self._as.set_succeeded(result)
+    else:
+      rospy.loginfo("Gagal")
+      self._as.set_aborted(result)
+
+if __name__ == '__main__':
+  rospy.init_node('count_until_server_goal')
+  
+  server = CountUntilServer()
+
+  rospy.spin()
